052fb549d479a61fed4edd93d0423c1b36c92cf0
Brett Clark <breclark@zendesk.com>
2019-03-03 17:13:05 +1100

    Init commit.

---
+++ data/process_data.py
@@ -0,0 +1,70 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    # Load messages and categories.
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    # Merge the 2 dataframes.
+    df = messages.merge(categories, on='id')
+    
+    # Split the categories column into many columns.
+    categories = pd.DataFrame(df['categories'].str.split(';', expand=True))
+    
+    # Set the column names for the categories.
+    cols = categories.loc[0].str.split('-', expand=True)[0].values
+    categories.columns = cols
+    
+    # Set each value to a numeric.
+    for category in categories:
+        # Set each value to the last character of the string.
+        categories[category] = categories[category].str[-1]
+
+        # Convert to numeric.
+        categories[category] = pd.to_numeric(categories[category])
+        
+    # Replace the old categories column.
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    
+    return df
+
+def clean_data(df):
+    # Remove duplicates.
+    return df.drop_duplicates()
+
+def save_data(df, database_filename):
+    engine = create_engine(f"sqlite:///{database_filename}")
+    df.to_sql('messages', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,93 @@
+import sys
+import pickle
+import re
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+def load_data(database_filepath):
+    # Load data from the database.
+    engine = create_engine(f"sqlite:///{database_filepath}")
+    conn = engine.connect()
+    df = pd.read_sql_table('messages', con=conn)
+    
+    # Split into features and targets.
+    X = df.message.values
+    y = df.as_matrix(columns=df.iloc[:, 4:].columns)
+    categories = np.unique(y)
+    
+    return X, y, categories
+
+
+def tokenize(text):
+    # Convert to lower case.
+    text = text.lower()
+
+    # Remove non-alphanumeric characters.
+    text = re.sub(r'[^a-z0-9]', ' ', text)
+
+    # Split into tokens.
+    tokens = text.split()
+    
+    return tokens
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(estimator=RandomForestClassifier()))
+    ])
+    
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    # Get predictions.
+    y_pred = model.predict(X_test)
+
+    # Check results for each output class.
+    for i, _ in enumerate(category_names):
+        print(classification_report(Y_test[i], Y_test[i]))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
